20fa529ecd4ff24acdef96266080a67c8259d62c
T-101 <T-101@PC237>
2018-10-01 19:40:36 -0500

    Imc

---
+++ src/imc/Imc.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imc;
+
+/**
+ *
+ * @author T-101
+ */
+public class Imc {
+    float valor;
+    void calcularImc(Persona p){
+        valor  = p.peso/(p.altura*p.altura);
+    }
+    
+}
