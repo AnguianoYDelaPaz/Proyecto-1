77d259cf64d8f8fd4034b27db27c0f9108717e93
T-101 <T-101@PC237>
2018-10-04 19:37:43 -0500

    Arreglos2

---
+++ src/tipos1/Arreglos2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipos1;
+
+/**
+ *
+ * @author T-101
+ */
+public class Arreglos2 {
+    public static void main(String[] args) {
+        int x;
+        int x2 [] = new int [2];
+        System.out.println(x2[0]);
+        String hola[]={"Ya", "casi", "nos", "vamos"};
+        for (int i = 0; i < hola.length; i++)
+        {
+            System.out.println(hola[i]);
+        }
+        //ciclo for mejorado
+        for (String y :hola)
+        {
+            System.out.println(y);
+        }
+    }
+}
