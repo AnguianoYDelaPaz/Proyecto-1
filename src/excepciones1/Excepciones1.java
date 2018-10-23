b7399670bd129d73928d0e1ea28b8b9a233409f8
T-101 <T-101@PC237>
2018-10-22 19:22:31 -0500

    Excepciones

---
+++ src/excepciones1/Excepciones1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones1;
+
+/**
+ *
+ * @author T-101
+ */
+public class Excepciones1 {
+    public static void main(String[] args) {
+        //Unchecked Exception:
+    
+        float x = 0;
+        int y = 2;
+        System.out.println(y/x);
+       
+        //Checked Exception:
+        
+        //int a = 4;
+        //String b = "dos";
+        //int c = a + b;
+        //int bb = Integer.parseInt(b);
+        //int c = a + bb;
+        //System.out.println(c);
+        
+        //Arreglo de Strings:
+        
+        String mensa[] = {"Ya", "casi", "acabamos"};
+        //System.out.println(mensa[0]);
+        
+    }
+}
