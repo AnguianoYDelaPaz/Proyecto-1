2bae83f41b709d3c23a4a45f50462099aa3f1bd3
T-101 <T-101@PC237>
2018-11-01 19:48:14 -0600

    Examen Practico

---
+++ src/examenpractico/Tarjeta.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenpractico;
+
+/**
+ *
+ * @author T-101
+ */
+public class Tarjeta {
+    private int numero;
+    private float saldo;
+
+    public Tarjeta() {
+        
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+    
+    
+}
