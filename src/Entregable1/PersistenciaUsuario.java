add7c98db917f9d92f6463ff89c355aa7dc62145
T-101 <T-101@PC237>
2018-11-08 19:45:29 -0600

    Entregable 1

---
+++ src/Entregable1/PersistenciaUsuario.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entregable1;
+
+/**
+ *
+ * @author T-101
+ */
+public class PersistenciaUsuario {
+    public static void main(String[] args) {
+        
+    }
+ 
+}
---
+++ src/Entregable1/Usuario.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entregable1;
+import java.io.*;
+/**
+ *
+ * @author T-101
+ */
+public class Usuario implements Serializable {
+    
+}
