f973ccf6d300dacb14f4f06a3bc5251eebdd04ae
T-101 <T-101@PC237>
2018-11-12 19:43:34 -0600

    Entregable1

--- src/Entregable1/PersistenciaUsuario.java
+++ src/Entregable1/PersistenciaUsuario.java
@@ -4,14 +4,33 @@
  * and open the template in the editor.
  */
 package Entregable1;
-
+import java.io.*;
+import java.util.ArrayList;
 /**
  *
  * @author T-101
  */
 public class PersistenciaUsuario {
-    public static void main(String[] args) {
-        
+    ArrayList<Usuario> usuarios;
+
+    public PersistenciaUsuario() {
+        usuarios = new ArrayList<>();
+    }
+    public void guardar (Usuario u)
+    throws Exception{
+        File f = new File ("usuarios");
+        if(f.exists())usuarios = leerTodos();
+        FileOutputStream fos = new FileOutputStream(f);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        usuarios.add(u);
+        oos.writeObject(usuarios);
+    }
+    public ArrayList <Usuario> leerTodos()throws Exception{
+        File f = new File ("usuarios");
+        FileInputStream fis = new FileInputStream(f);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        usuarios = (ArrayList<Usuario>)ois.readObject();
+        return usuarios;
     }
  
 }
